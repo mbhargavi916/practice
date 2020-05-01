d850d25311274fedc0eefdfb0322521cfd8a6aeb
mbhargavi <mbhargavi916@gmail.com>
2020-05-01 16:34:25 +0200

    frist try for pull request

---
+++ app.js
@@ -0,0 +1,42 @@
+console.log(__dirname);
+console.log(__filename);
+console.log("Hello World");
+var first_name = "Bhargavi";
+
+console.log(first_name);
+var a = 20;
+var b = 40;
+var sum = a + b;
+console.log(sum);
+if (a == b) {
+  console.log("It is true");
+} else {
+  console.log("IT is not true");
+}
+
+function greet() {
+  console.log("Hello world");
+}
+greet();
+//coments
+
+var x = 8;
+var y = 4;
+var z = x + y;
+function add() {
+  console.log(z);
+  console.log("hi");
+}
+add();
+var name = "bhargavi";
+var last = "Manchikalapudi";
+function fullname() {
+  console.log(" My name is " + name + last);
+}
+fullname();
+var f = 10;
+{
+  console.log("f");
+}
+
+//coment
